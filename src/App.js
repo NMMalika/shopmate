bd9b7ef4e8c66974491b54d2a3fce2146cfdd25d
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-02-18 18:57:01 +0300

    useEffects, useCallback and Custom Hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ProductList } from "./components/ProductList"; 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Products  List</h1>
+      <ProductList />
+      
+    </div>
+  );
+}
+
+export default App;
